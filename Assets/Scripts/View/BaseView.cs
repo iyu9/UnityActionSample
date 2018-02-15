00c913ef8a60e2278e1e3065dc31c7f53e3f207d
iyu9 <tomii.yuto@gmail.com>
2018-02-15 09:48:45 +0900

    update_all

---
+++ Assets/Scripts/View/BaseView.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// 基底ビュークラス
+/// </summary>
+public class BaseView : MonoBehaviour
+{
+  protected void Awake()
+  {
+    Debug.Log("BaseView.Awake");
+  }
+
+  protected void Start()
+  {
+    Debug.Log("BaseView.Start");
+  }
+
+  protected void Open()
+  {
+    Debug.Log("BaseView.Open");
+  }
+
+  protected void Close()
+  {
+    Debug.Log("BaseView.Close");
+  }
+}
